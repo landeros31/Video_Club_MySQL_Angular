e437dcaeab788b55a1e48404f8e53fd2cc86f238
Sebastian Landeros <sebastianlanderos1@gmail.com>
2021-09-28 21:23:13 -0500

    Page found

--- server/config/conexion.js
+++ server/config/conexion.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 const conexion =mysql.createConnection({
 
-host : 'localhost',
-user: 'root',
-password : 'sebastian31',
-port: 3306,
-database : 'movie'
-
-});
-
+  host : 'localhost',
+  user: 'root',
+  password : 'sebastian31',
+  port: 3306,
+  database : 'movie'
+  
+  });
+  
 conexion.connect((err)=>{
   if(err){
     console.log("ha ocurrido un error error"+err)
