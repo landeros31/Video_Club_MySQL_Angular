b52555d885711c804e4c5a110ece937734c565c9
Sebastian Landeros <sebastianlanderos1@gmail.com>
2021-09-28 22:24:15 -0500

    server running

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.set('port',port)
 
 //rutas
-app.use('/api', require('../server/rutas'))
+app.use('/', require('../server/rutas'))
 
 /////iniciar express
 app.listen(app.get('port'),(error)=>{
